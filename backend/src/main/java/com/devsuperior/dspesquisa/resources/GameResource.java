aa790208278711ecaeab33b7912bb4f53ecd262c
FernandaBroch <fernanda.broch@gmail.com>
2020-09-15 16:53:49 -0300

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
@@ -1,11 +1,8 @@
 package com.devsuperior.dspesquisa.resources;
 
 import com.devsuperior.dspesquisa.dto.GameDTO;
-import com.devsuperior.dspesquisa.entities.Game;
-import com.devsuperior.dspesquisa.repositories.GameRepository;
 import com.devsuperior.dspesquisa.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
